5794cb995cb453e58fecacf48207e9d5b4e760d0
Milan <milan.dhar@gmail.com>
2019-06-04 16:03:40 -0700

    added dropdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  document.querySelector("#create-task-form").addEventListener('submit', (event) => {
+    event.preventDefault();
+    let newTask = event.target[0].value;
+    let priority = event.target[1].value;
+    console.log(event.target[0].value);
+    addTaskToList(newTask, priority);
+    event.target[0].value = "";
+
+  })
+
+  function addTaskToList(task, priority) {
+    const taskList = document.querySelector('#tasks');
+    let newTask = document.createElement("li");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "D"
+    deleteBtn.id = "delete-btn"
+    newTask.className = "taskItems"
+    newTask.textContent = task;
+    console.log
+    newTask.style.color = priority;
+    newTask.appendChild(deleteBtn);
+    taskList.appendChild(newTask);
+    deleteBtn.addEventListener('click', () => {
+      newTask.parentNode.removeChild(newTask)
+    })
+
+  }
+
+
+
 });
